dca822afa00385eaf79d8687bf23360b0d036e60
= <haddonkorinek@gmail.com>
2015-11-07 10:16:42 -0600

    Project 4: Implemented Multiplication in assembly.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//initialize product to zero (R2)
+//while R0 > 0
+    //R2 += R1
+    //R0 = R0-1
+
+    @2
+    M=0
+(LOOP)
+    @0
+    D=M         //D = R0
+    @END
+    D;JLE       //Exit the loop if R0 <= 0
+
+    @1
+    D=M         //D = R1
+    @2
+    M=M+D       //R2 += R1
+
+    @0
+    M=M-1       //Decrement R0
+
+    @LOOP
+    0;JMP // GoTo Loop
+(END)
+    @END
+    0;JMP // Infinite loop
